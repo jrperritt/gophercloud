2189e4b64fa5babd360a16e90503d0e31af543a4
Jon Perritt <jrperritt@gmail.com>
2014-07-01 11:35:03 -0500

    define image fields

--- openstack/imageservice/v1/images/images.go
+++ openstack/imageservice/v1/images/images.go
@@ -1,6 +1,24 @@
 package images
 
-type Image map[string]interface{}
+type Image struct {
+	Status           string
+	Container_format string
+	Created_at       string
+	Deleted_at       string
+	Properties       map[string]interface{}
+	Is_public        bool
+	Size             int
+	Name             string
+	Deleted          bool
+	Disk_format      string
+	Updated_at       string
+	Id               string
+	Checksum         string
+	Owner            string
+	Min_disk         int
+	Protected        bool
+	Min_ram          int
+}
 
 type ListOpts struct {
 	Params map[string]string
